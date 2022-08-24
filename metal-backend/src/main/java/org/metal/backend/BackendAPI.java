aeebe51ae909a9e789603a88e5da6cbb27553cf6
cheney.yin <cheneyyin@hotmail.com>
2022-08-24 15:26:36 +0800

    Add Backend REST API.

--- metal-backend/src/main/java/org/metal/backend/BackendAPI.java
+++ metal-backend/src/main/java/org/metal/backend/BackendAPI.java
@@ -1,35 +1,101 @@
 package org.metal.backend;
 
-import io.netty.handler.codec.http.HttpResponse;
-import io.netty.handler.codec.http.HttpServerCodec;
 import io.vertx.core.*;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
-import io.vertx.core.http.impl.HttpResponseHead;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.shareddata.LocalMap;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.metal.draft.Draft;
 import org.metal.draft.DraftMaster;
 import org.metal.exception.MetalAnalysedException;
+import org.metal.exception.MetalExecuteException;
 import org.metal.specs.Spec;
 import org.metal.specs.SpecFactoryOnJson;
 
 import java.io.IOException;
-import java.nio.Buffer;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+enum BackendAPIStatus {
+  UPPING,
+  LIGHT,
+  ANALYSING,
+  EXECUTING,
+  DOWNING
+}
+
 public class BackendAPI extends AbstractVerticle {
 
+  private String id;
   private IBackend backend;
   private HttpServer server;
-  private Router router;
 
-  public BackendAPI(IBackend backend) {
+  private AtomicInteger status = new AtomicInteger(0);
+
+  public BackendAPI(String id, IBackend backend) {
+    this.id = id;
     this.backend = backend;
+  }
+
+  private void restAdmit(RoutingContext ctx) {
+    if (status.get() == BackendAPIStatus.UPPING.ordinal()) {
+      JsonObject resp = new JsonObject();
+      resp.put("status", "FAIL");
+      resp.put("msg", "Backend is upping. The request should wait a moment.");
+      String payload = resp.toString();
+      ctx.response()
+          .putHeader("content-type", ctx.getAcceptableContentType())
+          .putHeader("content-length", String.valueOf(payload.length()))
+          .end(payload);
+    } else if (status.get() == BackendAPIStatus.DOWNING.ordinal()) {
+      JsonObject resp = new JsonObject();
+      resp.put("status", "FAIL");
+      resp.put("msg", "Backend is downing.");
+      String payload = resp.toString();
+      ctx.response()
+          .putHeader("content-type", ctx.getAcceptableContentType())
+          .putHeader("content-length", String.valueOf(payload.length()))
+          .end(payload);
+    } else {
+      ctx.next();
+    }
+  }
+
+  private void execAdmit(RoutingContext ctx) {
+    if (status.compareAndSet(BackendAPIStatus.LIGHT.ordinal(),
+        BackendAPIStatus.EXECUTING.ordinal())) {
+      ctx.next();
+    } else {
+      JsonObject resp = new JsonObject();
+      resp.put("status", "FAIL");
+      resp.put("msg", "The request of execution should wait a moment.");
+      String payload = resp.toString();
+      ctx.response()
+          .putHeader("content-type", ctx.getAcceptableContentType())
+          .putHeader("content-length", String.valueOf(payload.length()))
+          .end(payload);
+    }
+  }
+
+  private void specAdmit(RoutingContext ctx) {
+    if (status.compareAndSet(BackendAPIStatus.LIGHT.ordinal(),
+        BackendAPIStatus.ANALYSING.ordinal())) {
+      ctx.next();
+    } else {
+      JsonObject resp = new JsonObject();
+      resp.put("status", "FAIL");
+      resp.put("msg", "The request of analysis should wait a moment.");
+      String payload = resp.toString();
+      ctx.response()
+          .putHeader("content-type", ctx.getAcceptableContentType())
+          .putHeader("content-length", String.valueOf(payload.length()))
+          .end(payload);
+    }
   }
 
   private void spec(RoutingContext ctx) {
@@ -43,25 +109,29 @@ public class BackendAPI extends AbstractVerticle {
       List<String> analysed = backend.service().analysed();
       List<String> unanalysed = backend.service().unAnalysed();
       resp = new JsonObject();
-      resp.put("analysed", analysed)
-          .put("unanalysed", unanalysed)
-          .put("status", "success");
+      resp.put("status", "OK")
+          .put("data",
+              new JsonObject()
+                  .put("analysed", analysed)
+                  .put("unanalysed", unanalysed)
+          );
 
     } catch (IOException e) {
-      e.printStackTrace();
-      resp.put("status", "error").put("msg", "Fail to parse spec your post.");
+      resp.put("status", "FAIL").put("msg", "Fail to parse spec your post.");
     } catch (NullPointerException | NoSuchElementException | IllegalArgumentException e) {
-      e.printStackTrace();
-      resp.put("status", "error").put("msg", "Fail to parse spec your post.");
+      resp.put("status", "FAIL").put("msg", "Fail to parse spec your post.");
     } catch (MetalAnalysedException e) {
-      e.printStackTrace();
-      resp.put("status", "error").put("msg", "Fail to analyse metal.");
+      resp.put("status", "FAIL").put("msg", "Fail to analyse metal.");
     } finally {
       String payload = resp.toString();
       ctx.response()
           .putHeader("content-type", ctx.getAcceptableContentType())
           .putHeader("content-length", String.valueOf(payload.length()))
           .end(payload);
+      if (!status.compareAndSet(BackendAPIStatus.ANALYSING.ordinal(),
+          BackendAPIStatus.LIGHT.ordinal())) {
+        System.out.println("STATUS ILLEGE.");
+      }
     }
   }
 
@@ -69,6 +139,7 @@ public class BackendAPI extends AbstractVerticle {
     router.route(HttpMethod.POST, "/spec")
         .produces("application/json")
         .handler(BodyHandler.create())
+        .handler(this::specAdmit)
         .blockingHandler(this::spec, true);
   }
 
@@ -84,53 +155,96 @@ public class BackendAPI extends AbstractVerticle {
   }
 
   private void execRestApi(Router router) {
-      router.route(HttpMethod.POST, "/execution")
-          .produces("application/json")
-          .handler((RoutingContext ctx) -> {
-              JsonObject resp = new JsonObject();
-              boolean isExec = true;
-              if (backend.service().analysed().isEmpty()) {
-                  isExec = false;
-                  resp.put("status", "FAIL")
-                          .put("msg", "Not any analysed metal in context.");
+    router.route(HttpMethod.POST, "/execution")
+        .produces("application/json")
+        .handler(this::execAdmit)
+        .handler((RoutingContext ctx) -> {
+          JsonObject resp = new JsonObject();
+          boolean isExec = true;
+          if (backend.service().analysed().isEmpty()) {
+            isExec = false;
+            resp.put("status", "FAIL")
+                .put("msg", "Not any analysed metal in context.");
 
-              }
-              if (!backend.service().unAnalysed().isEmpty()) {
-                  isExec = false;
-                  resp.put("status", "FAIL")
-                      .put("msg", "Some unAnalysed metals exist in context.");
-              }
+          }
+          if (!backend.service().unAnalysed().isEmpty()) {
+            isExec = false;
+            resp.put("status", "FAIL")
+                .put("msg", "Some unAnalysed metals exist in context.");
+          }
 
+          if (isExec) {
+            ctx.next();
+            resp.put("status", "OK");
+            resp.put("msg", "Success to execute.");
+          } else {
+            if (!status.compareAndSet(BackendAPIStatus.EXECUTING.ordinal(),
+                BackendAPIStatus.LIGHT.ordinal())) {
+              System.out.println("STATUS ILLEGE.");
+            }
+          }
 
-              if (isExec) {
-                  ctx.next();
-                  resp.put("status", "Success");
-                  resp.put("msg", "Success to execute.");
-              }
+          String payload = resp.toString();
+          ctx.response()
+              .putHeader("content-type", ctx.getAcceptableContentType())
+              .putHeader("content-length", String.valueOf(payload.length()))
+              .end(payload);
 
-              String payload = resp.toString();
-              ctx.response()
-                  .putHeader("content-type", ctx.getAcceptableContentType())
-                  .putHeader("content-length", String.valueOf(payload.length()))
-                  .end(payload);
+        })
+        .blockingHandler((RoutingContext ctx) -> {
+          try {
+            backend.service().exec();
+          } catch (MetalExecuteException e) {
 
-          })
-          .blockingHandler((RoutingContext ctx) -> {
-              System.out.println("Call Exec.");
-              backend.service().exec();
-          }, true);
+          } finally {
+            if (!status.compareAndSet(BackendAPIStatus.EXECUTING.ordinal(),
+                BackendAPIStatus.LIGHT.ordinal())) {
+              System.out.println("STATUS ILLEGE.");
+            }
+          }
+        }, true);
+  }
+
+  private void identify(RoutingContext ctx) {
+    String pathID = ctx.pathParam("id");
+    if (!pathID.equals(id)) {
+      JsonObject resp = new JsonObject();
+      resp.put("status", "FAIL");
+      resp.put("msg", pathID + " is NOT FOUND.");
+      String payload = resp.toString();
+      ctx.response()
+          .putHeader("content-type", ctx.getAcceptableContentType())
+          .putHeader("content-length", String.valueOf(payload.length()))
+          .setStatusCode(404)
+          .end(payload);
+    } else {
+      ctx.next();
+    }
   }
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     HttpServerOptions options = new HttpServerOptions();
     server = getVertx().createHttpServer(options);
-    router = Router.router(getVertx());
-    heartRestApi(router);
-    specRestApi(router);
-    execRestApi(router);
 
-    server.requestHandler(router);
+    Router metalAPI = Router.router(getVertx());
+    heartRestApi(metalAPI);
+    specRestApi(metalAPI);
+    execRestApi(metalAPI);
+
+    Router identifyAPI = Router.router(getVertx());
+    identifyAPI.route("/:id*")
+        .produces("application/json")
+        .handler(this::identify)
+        .subRouter(metalAPI);
+
+    Router restAPIv1 = Router.router(getVertx());
+    restAPIv1.route("/v1*")
+        .handler(this::restAdmit)
+        .subRouter(identifyAPI);
+
+    server.requestHandler(restAPIv1);
+
     server.listen(18000)
         .onFailure(t -> {
           System.out.println("Fail to create http server.");
@@ -138,12 +252,14 @@ public class BackendAPI extends AbstractVerticle {
         })
         .onSuccess(srv -> {
           System.out.println("Success to create http server.");
+          status.set(BackendAPIStatus.LIGHT.ordinal());
           startPromise.complete();
         });
   }
 
   @Override
   public void stop(Promise<Void> stopPromise) throws Exception {
+    status.set(BackendAPIStatus.DOWNING.ordinal());
     server.close().onFailure(t -> {
       System.out.println("Fail to close http server.");
       stopPromise.fail(t);
@@ -162,7 +278,8 @@ public class BackendAPI extends AbstractVerticle {
     VertxOptions vertxOptions = new VertxOptions();
     DeploymentOptions deploymentOptions = new DeploymentOptions();
     Vertx vertx = Vertx.vertx(vertxOptions);
-    BackendAPI api = new BackendAPI(backend);
+    String id = "aaaaa-zzzzz";
+    BackendAPI api = new BackendAPI(id, backend);
 
     deploymentOptions.setWorker(false).setInstances(1);
     vertx.deployVerticle(api, deploymentOptions)
