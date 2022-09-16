e0ae216929477004ac51367442e92e62886622d6
cheney.yin <cheneyyin@hotmail.com>
2022-09-16 16:14:11 +0800

    Add addFromManifest

--- metal-server/src/main/java/org/metal/server/repo/MetalRepoDB.java
+++ metal-server/src/main/java/org/metal/server/repo/MetalRepoDB.java
@@ -1,10 +1,15 @@
 package org.metal.server.repo;
 
 import io.vertx.core.Future;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.streams.ReadStream;
+import io.vertx.ext.mongo.BulkOperation;
 import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.mongo.MongoClientBulkWriteResult;
 import io.vertx.ext.mongo.MongoClientDeleteResult;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class MetalRepoDB {
@@ -29,14 +34,12 @@ public class MetalRepoDB {
         .put("version", s[2]);
   }
 
-  public static Future<String> add(
-      MongoClient mongo, String userId,
+  private static JsonObject metalOf(String userId,
       MetalType type,
       MetalScope scope,
       String pkg, String clazz,
       JsonObject formSchema, Optional<JsonObject> uiSchema
-      ) {
-
+  ) {
     JsonObject metal = new JsonObject()
         .put("userId", userId)
         .put("type", type.toString())
@@ -55,8 +58,69 @@ public class MetalRepoDB {
     uiSchema.ifPresent(u -> {
       metal.put("uiSchema", u);
     });
+    return metal;
+  }
 
+  public static Future<String> add(
+      MongoClient mongo, String userId,
+      MetalType type,
+      MetalScope scope,
+      String pkg, String clazz,
+      JsonObject formSchema, Optional<JsonObject> uiSchema
+      ) {
+    JsonObject metal = metalOf(userId, type, scope, pkg, clazz, formSchema, uiSchema);
     return mongo.insert(DB, metal);
+  }
+
+  private static BulkOperation wrapBulkOperation(JsonObject metalRaw, String userId, MetalType type) {
+    MetalScope scope = MetalScope.valueOf(metalRaw.getString("scope", "PUBLIC"));
+    JsonObject metal = metalOf(
+        userId, type, scope,
+        metalRaw.getString("pkg"),
+        metalRaw.getString("clazz"),
+        metalRaw.getJsonObject("formSchema"),
+        Optional.ofNullable(metalRaw.getJsonObject("uiSchema"))
+    );
+    return BulkOperation.createInsert(metal);
+  }
+
+  public static Future<MongoClientBulkWriteResult> addFromManifest(
+      MongoClient mongo,
+      String userId,
+      JsonObject manifest
+  ) {
+    List<BulkOperation> operations = new ArrayList<>();
+    JsonArray sources = manifest.getJsonArray("sources");
+    sources.stream().map(obj -> (JsonObject)obj)
+        .map((JsonObject metalRaw) -> {
+          return wrapBulkOperation(metalRaw, userId, MetalType.SOURCE);
+        }).forEach(operations::add);
+
+    JsonArray mappers = manifest.getJsonArray("mappers");
+    mappers.stream().map(obj -> (JsonObject)obj)
+        .map((JsonObject metalRaw) -> {
+          return wrapBulkOperation(metalRaw, userId, MetalType.MAPPER);
+        }).forEach(operations::add);
+
+    JsonArray fusions = manifest.getJsonArray("fusions");
+    fusions.stream().map(obj -> (JsonObject)obj)
+        .map((JsonObject metalRaw) -> {
+          return wrapBulkOperation(metalRaw, userId, MetalType.FUSION);
+        }).forEach(operations::add);
+
+    JsonArray sinks = manifest.getJsonArray("sinks");
+    sinks.stream().map(obj -> (JsonObject)obj)
+        .map((JsonObject metalRaw) -> {
+          return wrapBulkOperation(metalRaw, userId, MetalType.SINK);
+        }).forEach(operations::add);
+
+    JsonArray setups = manifest.getJsonArray("setups");
+    setups.stream().map(obj -> (JsonObject)obj)
+        .map((JsonObject metalRaw) -> {
+          return wrapBulkOperation(metalRaw, userId, MetalType.SETUP);
+        }).forEach(operations::add);
+
+    return mongo.bulkWrite(DB, operations);
   }
 
   public static Future<JsonObject> get(MongoClient mongo, String userId, String metalId) {
